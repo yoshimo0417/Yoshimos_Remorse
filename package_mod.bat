777f43ab0c5eaf6aec9d0a67cf455a1377b14358
CamDawg <pcamagna@yahoo.com>
2025-12-23 09:50:10 -0800

    Adding PI hooks, prepping v3.2

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=Yoshimo's Remorse"
-set mod_version=v3.1.1
+set mod_version=v3.2
 set mod_folder=yoshimosremorse
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
